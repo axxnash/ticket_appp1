3396a4479669971ab8212f2bb9d27ac62faf5dfc
axxnash <abinashrajasegaran@gmail.com>
2025-05-12 16:35:48 +0800

    first change

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  //hi
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: BottomNavBar());
